48e89b16685bf745a445f311d0d30f5932588367
Antonio Junior <junior.fragalevi95@gmail.com>
2025-09-23 23:19:56 -0300

    Meu primeiro commit

---
+++ chatbot.js
@@ -0,0 +1,132 @@
+// leitor de qr code
+const qrcode = require('qrcode-terminal');
+const { Client, Buttons, List, MessageMedia } = require('whatsapp-web.js'); // Mudança Buttons
+const client = new Client();
+
+// serviço de leitura do qr code
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+// apos isso ele diz que foi tudo certo
+client.on('ready', () => {
+    console.log('Tudo certo! WhatsApp conectado.');
+});
+
+// E inicializa tudo 
+client.initialize();
+
+const delay = ms => new Promise(res => setTimeout(res, ms)); // Função que usamos para criar o delay entre uma ação e outra
+
+// Funil
+
+client.on('message', async msg => {
+
+    if (msg.body.match(/(menu|Menu|dia|tarde|noite|oi|Oi|Olá|olá|ola|Ola)/i) && msg.from.endsWith('@c.us')) {
+
+        const chat = await msg.getChat();
+
+        await delay(3000); //delay de 3 segundos
+        await chat.sendStateTyping(); // Simulando Digitação
+        await delay(3000); //Delay de 3000 milisegundos mais conhecido como 3 segundos
+        const contact = await msg.getContact(); //Pegando o contato
+        const name = contact.pushname; //Pegando o nome do contato
+        await client.sendMessage(msg.from,'Olá! '+ name.split(" ")[0] + 'Sou o assistente virtual da Empresa NetPonte. Como posso ajudá-lo hoje? Por favor, digite uma das opções abaixo:\n\n1 - Quem somos\n2 - Conheça nossos planos\n3 - 2 via de boleto\n4 - Falar com atendente\n5 - Outras perguntas'); //Primeira mensagem de texto
+        await delay(3000); //delay de 3 segundos
+        await chat.sendStateTyping(); // Simulando Digitação
+        await delay(5000); //Delay de 5 segundos
+    
+        
+    }
+
+//Quem somos
+
+    if (msg.body !== null && msg.body === '1' && msg.from.endsWith('@c.us')) {
+        const chat = await msg.getChat();
+
+
+        await delay(3000); //delay de 3 segundos
+        await chat.sendStateTyping(); // Simulando Digitação
+        await delay(3000);
+        await client.sendMessage(msg.from, 'Somos a NetPonte, provedor de internet 100%  fibra óptica, levando conexão rápida e estável para região. Nosso objetivo é manter você sempre online, com suporte humanizado e planos sob medida para sua necessidade. \n');
+
+        await delay(3000); //delay de 3 segundos
+        await chat.sendStateTyping(); // Simulando Digitação
+        await delay(3000);
+        await client.sendMessage(msg.from, 'Nossa sede fica na Rua Guilherme de Abreu, Bairro Ponte, 1794A\nTelefone para contato (99) 98119-0228.\n\nÉ um prazer em servi-los!');
+        
+        await sendMainMenu(chat, contact); // Retorna ao menu
+       
+
+
+    }
+
+// Nossos Planos
+    
+    if (msg.body !== null && msg.body === '2' && msg.from.endsWith('@c.us')) {
+        const chat = await msg.getChat();
+
+
+        await delay(3000); //Delay de 3000 milisegundos mais conhecido como 3 segundos
+        await chat.sendStateTyping(); // Simulando Digitação
+        await delay(3000);
+        await client.sendMessage(msg.from, '*CONHEÇA NOSSOS PLANOS:*\n\n*Plano 400 MEGAS:* R$88,00 por mês.\n\n*Plano 500 MEGAS:* R$90,00 por mês.\n\n*Plano 600 MEGAS:* R$100,00 por mês.\n\n*Plano 700 MEGAS:* R$120,00 por mês.');
+
+        await delay(3000); //delay de 3 segundos
+        await chat.sendStateTyping(); // Simulando Digitação
+        await delay(3000);
+        await client.sendMessage(msg.from, 'Instalação passivel de gratuidade.\nFornecemos aparelhos em comodato');
+
+        await delay(3000); //delay de 3 segundos
+        await chat.sendStateTyping(); // Simulando Digitação
+        await delay(3000);
+        await client.sendMessage(msg.from, '*Incrível né?.*');
+    
+    
+
+        await delay(3000); //delay de 3 segundos
+        await chat.sendStateTyping(); // Simulando Digitação
+        await delay(3000);
+        await client.sendMessage(msg.from,"Qual dos planos você se interessou?");
+    }
+
+   
+//sagunda via de boleto
+    
+    if (msg.body !== null && msg.body === '3' && msg.from.endsWith('@c.us')) {
+        const chat = await msg.getChat();
+
+
+        await delay(3000); //Delay de 3000 milisegundos mais conhecido como 3 segundos
+        await chat.sendStateTyping(); // Simulando Digitação
+        await delay(3000);
+        await client.sendMessage(msg.from, '*Para segunda via de boleto precisamos que informe seu CPF.*\n\nDigite o número do seu CPF:');
+        
+    }
+
+// Falar com Atendentes
+
+    if (msg.body !== null && msg.body === '4' && msg.from.endsWith('@c.us')) {
+        const chat = await msg.getChat();
+
+        await delay(3000); //Delay de 3000 milisegundos mais conhecido como 3 segundos
+        await chat.sendStateTyping(); // Simulando Digitação
+        await delay(3000);
+        await client.sendMessage(msg.from, '*Sua mensagem será encaminhada a um de nossos atentendes.*');
+
+    }
+
+// Tirar Duvidas
+
+    if (msg.body !== null && msg.body === '5' && msg.from.endsWith('@c.us')) {
+        const chat = await msg.getChat();
+
+        await delay(3000); //Delay de 3000 milisegundos mais conhecido como 3 segundos
+        await chat.sendStateTyping(); // Simulando Digitação
+        await delay(3000);
+        await client.sendMessage(msg.from, 'Deixe aqui sua duvida, em até 10 minutos retornaremos com a resposta! ');
+
+
+    }
+
+});
